36406b78971ac982df555ed66ca2d49c66f5966a
MatthiasPilz <Matthias.Pilz@kingston.ac.uk>
2019-11-30 14:53:13 +0000

    extract payment method

--- inter.py
+++ inter.py
@@ -1,1 +1,15 @@
 # python for interpreting the json response
+import json
+
+with open('example.txt') as json_file:
+    data = json.load(json_file)
+
+    for feature in data['features']:
+        prop = feature['properties']
+        reg = prop['regulations']
+        if ( reg[0]['rule']['payment'] ):
+            if ( reg[0]['payment'] ):
+                pay = reg[0]['payment']
+                print(pay['methods'])
+
+
